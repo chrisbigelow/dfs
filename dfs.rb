6acbf48f42fc1198e8c8aeb0631d4ed993cf01f5
Christopher Bigelow <chrisbigelow25@gmail.com>
2018-01-20 12:19:45 -0800

    Add Depth First Search" "

--- dfs.rb
+++ dfs.rb
@@ -1,5 +1,7 @@
 class Node 
 
+attr_accessor :value, :left, :right
+
 def initialize(value)
   @value = value
   @left = nil
@@ -33,24 +35,30 @@ child_one.right = second_child_two
 
 child_two_child_one = Node.new(6)
 child_two_child_two = Node.new(7)
-left = Node.new(8)
+left_node = Node.new(8)
 child_two.right = child_two_child_one
-child_two.left = left
+child_two.left = left_node
 child_two_child_one.right = child_two_child_two
 
 
 #put in root and target value
 
 def dfs(node, target)
-  return nil if node.nil?
   return node if node.value == target
-  left = dfs(node.left, target) if node.left
-  right = dfs(node.right, target) if node.right
-  left || right
+  return nil if node.children.empty?
+  node.children.each do |n|
+    result = dfs(n, target)
+    return result unless result.nil? 
+  end
+  nil
 end
 
+res = dfs(root, 8)
+puts "Depth First Search Result: " + res.to_s
+puts "Value: " + res.value.to_s
 
-p dfs(root, 7)
+
+
 
 
 
